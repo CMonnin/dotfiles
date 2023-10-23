2bdbda2592e79a43940545625ed2c290ef2a57c2
Cian Monnin <cianmonnin@gmail.com>
2023-10-23 14:43:46 -0400

    fixed python LSP

--- nvim/lua/cmonnin/remap.lua
+++ nvim/lua/cmonnin/remap.lua
@@ -1,3 +1,4 @@
+-- in normal mode, leader pv will go to Explore :Ex
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+-- in insert mode jj Escapes to normal mode
 vim.keymap.set("i", "jj", "<Esc>")
--- nvim/lua/cmonnin/set.lua
+++ nvim/lua/cmonnin/set.lua
@@ -1,11 +1,12 @@
+-- set leader to space
 vim.g.mapleader = " "
-
+-- set relative line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.shiftwidth = 4 
+vim.shiftwidth = 4
 vim.expandtab = 4
 
 vim.opt.smartindent = true
@@ -13,8 +14,5 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 
 vim.opt.termguicolors = true
-
-
-
-
-
+-- keeps cursor in the middle of the screen when scrolling
+vim.o.scrolloff = 999
