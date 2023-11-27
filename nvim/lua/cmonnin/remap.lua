5da44ab2a962bdbcc8fc91a0453789f11e29e297
Cian Monnin <cianmonnin@gmail.com>
2023-11-27 16:18:42 -0500

    buffer remaps

--- nvim/lua/cmonnin/remap.lua
+++ nvim/lua/cmonnin/remap.lua
@@ -1,4 +1,7 @@
 -- in normal mode, leader pv will go to Explore :Ex
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>h", vim.cmd.bp)
+vim.keymap.set("n", "<leader>l", vim.cmd.bn)
 -- in insert mode jj Escapes to normal mode
 vim.keymap.set("i", "jj", "<Esc>")
