89a7b9b42f3053f70828a9c5c3595740ecf38f78
Cian Monnin <cianmonnin@gmail.com>
2024-08-01 13:44:05 -0400

    fix: deprecations fixed

--- nvim/lua/cmonnin/plugins/dap.lua
+++ nvim/lua/cmonnin/plugins/dap.lua
@@ -29,7 +29,7 @@ return {
 			-- }
 
 			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint, { desc = "dap: toggle_breakpoint" })
-			vim.keymap.set("n", "<space>gb", dap.run_to_cursor({ desc = "dap: run_to_cursor" }))
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor, { desc = "dap: run_to_cursor" })
 
 			-- Eval var under cursor
 			vim.keymap.set("n", "<space>?", function()
--- nvim/lua/cmonnin/plugins/lspconfig.lua
+++ nvim/lua/cmonnin/plugins/lspconfig.lua
@@ -39,19 +39,16 @@ return {
 
 			opts.desc = "Smart rename"
 			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
 			opts.desc = "Show buffer diagnostics"
 			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
 			opts.desc = "Show line diagnostics"
 			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 			opts.desc = "Go to previous diagnostic"
-			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+			keymap.set("n", "[d", vim.diagnostic.jump, { count = -1, float = true }) -- jump to previous diagnostic in buffer
 
 			opts.desc = "Go to next diagnostic"
-			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
+			keymap.set("n", "]d", vim.diagnostic.jump, { count = 1, float = true }) -- jump to next diagnostic in buffer
 			opts.desc = "Show documentation for what is under cursor"
 			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
@@ -61,13 +58,23 @@ return {
 
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
-
 		-- Change the Diagnostic symbols in the sign column (gutter)
 		-- (not in youtube nvim video)
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			-- vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			vim.diagnostic.config({
+				signs = {
+					text = {
+						[type] = icon,
+					},
+
+					linehl = {
+						[type] = hl,
+					},
+				},
+			})
 		end
 
 		-- configure python server
