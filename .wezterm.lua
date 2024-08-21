9947cfbee25d033aaf2edebc8add7dbc74b0c6fe
Cian Monnin <cianmonnin@gmail.com>
2024-08-21 09:44:13 -0400

    wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+-- Leader is the same as my old tmux prefix
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+  -- splitting
+  {
+    mods   = "LEADER",
+    key    = "-",
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods   = "LEADER",
+    key    = "|",
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+  }
+}
+config.font = wezterm.font('JetBrains Mono')
+config.dpi = 96
+config.color_scheme = 'Gruvbox Material (Gogh)'
+config.window_decorations = "RESIZE"
+return config
