551211cd7d60337f99a8c783e98b67051344fe83
Cian Monnin <cianmonnin@gmail.com>
2024-09-17 22:18:45 -0400

    added theme for slides 2

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,15 +2,15 @@ require("cmonnin")
 require("cmonnin.lazy")
 
 function Transparent_bg(color)
-	color = color or "gruvbox"
-	success, msg = pcall(vim.cmd, "colorscheme " .. color)
-	if not success then
-		print("Failed to load color scheme '" .. color .. "': " .. msg)
-		return
-	end
+  color = color or "gruvbox-material"
+  success, msg = pcall(vim.cmd, "colorscheme " .. color)
+  if not success then
+    print("Failed to load color scheme '" .. color .. "': " .. msg)
+    return
+  end
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 Transparent_bg()
