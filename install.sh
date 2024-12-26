0722cc0f2f0e31a737aeb41ba3ea55d9e82e7f9d
Cian <cianmonnin@gmail.com>
2024-12-26 11:49:58 -0500

    install script

--- install.sh
+++ install.sh
@@ -27,19 +27,22 @@ echo "✓ Wezterm configuration copied"
 echo "Change background (y/n)"
 read answer
 
-if ["$answer" = "y"] || ["$answer" = "Y"];
-then echo "Please enter path"
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+  echo "Please enter path"
   read image_path
-  if [-f "$image_path"]; then
-    echo "exec --no-startup-id feh --bg-scale $image_path" >> ./zsh/.zshrc
-    echo "background has been changed"
-    echo "source .zshrc to apply"
+
+  image_path="${image_path/#\~/$HOME}"
+
+  if [ -f "$image_path" ]; then
+    echo "exec --no-startup-id feh --bg-scale $image_path" >> ~/.zshrc
+    echo "Background has been changed."
+    echo "Source your .zshrc to apply the changes."
   else 
-    echo "file does not exist"
+    echo "File does not exist."
     exit 1
   fi
 else
-  echo "No changes made"
+  echo "No changes made."
 fi
 
 echo "Copying .zshrc configuration..."
