01066b5cc86abdb005f44ab200e84ce9370cd778
Cian Monnin <cianmonnin@gmail.com>
2025-01-10 16:56:07 -0500

    remap increment

--- nvim/lua/cmonnin/remap.lua
+++ nvim/lua/cmonnin/remap.lua
@@ -5,6 +5,10 @@ vim.keymap.set("n", "<leader>h", vim.cmd.bp, { desc = "buffer, previous" })
 vim.keymap.set("n", "<leader>l", vim.cmd.bn, { desc = "buffer, next" })
 -- in insert mode jj Escapes to normal mode
 vim.keymap.set("i", "jj", "<Esc>")
+-- remap increment to C-b as C-a is for wezterm/tmux leader
+vim.keymap.set("n", "<C-b>", "<C-a>")
+-- not wokring i visual mode for some reason
+vim.keymap.set("x", "<C-b>", "g<C-a>")
 
 -- map "replace all"
 vim.keymap.set("n", "<leader>s", ":%s/g<Left>", { desc = "replace all" })
