8d78644d622148466d647d5cd0410e562a1024d7
Cian Monnin <cianmonnin@gmail.com>
2025-01-22 14:19:42 -0500

    mason cleanups

--- nvim/lua/cmonnin/plugins/mason.lua
+++ nvim/lua/cmonnin/plugins/mason.lua
@@ -1,50 +1,48 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
-    -- enable mason
-    mason.setup({
-    })
+		local mason_tool_installer = require("mason-tool-installer")
+		-- enable mason
+		mason.setup({})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "svelte",
-        "pyright",
-        "ruff",
-        "gopls",
-        "eslint",
-        "rust_analyzer",
-        "dockerls",
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "graphql"
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua",   -- lua formatter
-        "isort",    -- python formatter
-        "black",    -- python formatter
-        "pylint",
-        "eslint_d",
-      },
-    })
-  end,
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"svelte",
+				"pyright",
+				"ruff",
+				"ruff_lsp",
+				"gopls",
+				"eslint",
+				"rust_analyzer",
+				"dockerls",
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"graphql",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"eslint_d",
+			},
+		})
+	end,
 }
